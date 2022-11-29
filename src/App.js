abecd6424d5ccbc6e29740286de2d315a5eef025
Maria <mariapokryshkina@gmail.com>
2022-11-29 23:11:40 +0200

    first excercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-const Privet = (props) => {
-  return(
-    <div>
-     <p>
-      Privet {props.name}, tebe {props.age} let!
-     </p> 
-   </div>
-  )
-}
-
 const App = () => {
-  const name = 'Maria'
-  const age = 30
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const part2 = 'Using props to pass data'
+  const part3 = 'State of a component'
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
 
-
+  const Header = (props) => {
   return (
     <div>
-      <h1>Greetings</h1>
-      <Privet name= {name} age={age} />
-      <Privet name= 'Alex' age = {30} />
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
-export default App;
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part1}</p>
+      <p>{props.part2}</p>
+      <p>{props.part3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>{props.exercises1}</p>
+      <p>{props.exercises2}</p>
+      <p>{props.exercises3}</p>
+    </div>
+  )
+}
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
+}
+
+export default App
+
+
