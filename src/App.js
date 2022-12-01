b41dbce33021acf1606faffda3a9538b14602903
Maria <mariapokryshkina@gmail.com>
2022-12-01 16:50:37 +0200

    1st and 2nd assignments are done

--- src/App.js
+++ src/App.js
@@ -15,35 +15,45 @@ const App = () => {
   )
 }
 
-const Content = (props) => {
+const Content = () => {
+  const Part = (props) => {
+    return (
+      <div> 
+        <p>{props.part1} {props.exercises1}</p>
+        <p>{props.part2} {props.exercises2}</p>
+        <p>{props.part3} {props.exercises3}</p>
+      </div>
+    )
+  }
   return (
     <div>
-      <p>{props.part1}</p>
-      <p>{props.part2}</p>
-      <p>{props.part3}</p>
+      <Part part1={part1} exercises1={exercises1}/>
+      <Part part1={part2} exercises1={exercises2}/>
+      <Part part1={part3} exercises1={exercises3}/>
     </div>
   )
 }
 
-const Total = (props) => {
-  return (
-    <div>
-      <p>{props.exercises1}</p>
-      <p>{props.exercises2}</p>
-      <p>{props.exercises3}</p>
-    </div>
-  )
-}
+// const Total = (props) => {
+//   return (
+//     <div>
+//       <p>{props.exercises1}</p>
+//       <p>{props.exercises2}</p>
+//       <p>{props.exercises3}</p>
+//     </div>
+//   )
+// }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content />
     </div>
   )
 }
 
 export default App
+
+
 
 
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        mainColor: '#050C2A'
+      },
+      padding: {
+        big: '50px'
+      },
+    },
+  },
+  plugins: [],
+}
